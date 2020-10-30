9e206af1db5ffd3552aa8974ba535012ebf62c1f
sgr45 <timalsinasagar4512@gmail.com>
2020-10-30 18:59:58 +0545

    Project initate

---
+++ cmake-build-debug/CMakeFiles/DSA_PROJECT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA_PROJECT.dir/main.cpp.o"
+  "DSA_PROJECT"
+  "DSA_PROJECT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSA_PROJECT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
